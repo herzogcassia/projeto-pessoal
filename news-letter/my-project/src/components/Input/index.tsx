47821ac1dbe9d71b9448da05c40e3d4aca31ee2d
cassia.herzog <cassiahherzog@gmail.com>
2022-12-12 16:38:38 -0300

    login em andamento

--- news-letter/my-project/src/components/Input/index.tsx
+++ news-letter/my-project/src/components/Input/index.tsx
@@ -5,21 +5,28 @@ export const Input = () => {
         <div className="pt-[80px] mt-[100px]  bg-purple-500 h-[500px] flex flex-col w-[300px] items-center justify-center">
             <form className='flex flex-col w-[300px] items-center justify-center'>
                 <div className=''>
-                    <label>
-                        Email:
+                    <div className=''>
+                        <p>
+                            Email:
+                        </p>
                         <input className='border-2 shadow-sm bg-abacate/40' />
-                    </label>
+                    </div>
                 </div>
                 <div>
-                    <label>
-                        Senha:
+                    <div>
+                        <p>
+                            Senha:
+                        </p>
                         <input className='border-2 shadow-sm bg-abacate/40' />
-                    </label>
-                </div> <div>
-                    <label>
-                        Entrar
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <p>
+                            Entrar
+                        </p>
                         <input className='border-2 shadow-sm bg-abacate/40' />
-                    </label>
+                    </div>
                 </div>
             </form>
         </div>
